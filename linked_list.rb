ffaf02186f5116783ed5e240011427932a763dae
Federico Esteban <federico.esteban@wolox.com.ar>
2018-08-10 11:58:36 -0300

    Implemented push method on LL.

--- linked_list.rb
+++ linked_list.rb
@@ -1,5 +1,5 @@
 class Node
-  attr_accessor :value
+  attr_accessor :value, :next
 
   def initialize(value)
     @value = value
@@ -8,9 +8,17 @@ class Node
 end
 
 class LinkedList
-  attr_reader :first
+  attr_accessor :first
 
   def initialize
     @first = nil
+  end
+
+  def push(value)
+    if @first == nil
+      @first = Node.new(value)
+    else
+      @first.next = Node.new(value)
+    end
   end
 end
